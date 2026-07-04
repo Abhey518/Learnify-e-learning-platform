4e95652cecc5b05459d38c289ed719169bdd0072
T M B M Thennakoon <142225271+Bhanuka2083@users.noreply.github.com>
2026-07-05 00:47:04 +0530

    Remove AppRoutes from App component
    Removed AppRoutes component from App function.

--- frontend/src/core/App.jsx
+++ frontend/src/core/App.jsx
@@ -1,26 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
-<<<<<<< HEAD
-import AppRoutes from './core/routes'
-=======
->>>>>>> 4ed59a1d7d2d56524150966236754f09e65a4059
 
 function App() {
   return (
     <Router>
       <div className="app">
-<<<<<<< HEAD
-        <AppRoutes /> 
-=======
         {/* App content */}
->>>>>>> 4ed59a1d7d2d56524150966236754f09e65a4059
       </div>
     </Router>
   )
 }
 
-<<<<<<< HEAD
 export default App
-=======
-export default App
->>>>>>> 4ed59a1d7d2d56524150966236754f09e65a4059
